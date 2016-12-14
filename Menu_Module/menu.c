965e92f79370b009ea5f79f97218f7671afe6fdb
frozenk1458 <frozenk@hotmail.fr>
2016-12-14 08:23:45 +0100

    Add files via upload

---
+++ Menu_Module/menu.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+/************************/
+/***                  ***/
+/*** Author : Frozenk ***/
+/***                  ***/
+/************************/
+
+#include <stdio.h>
+#include "main.h"
+
+void menu()
+{
+	char choice = ' ';
+	choice = ' ';
+	printf("\nYou are in the menu section but it is still under construction \n");
+	printf("\nPlease wait until finished. Thanks for your patience\n");
+	
+	printf("\n1. Display a number\n");
+	printf("\nQ. Quit the program\n");
+	
+	printf("\n Please tap your choice among the menu choices\n");
+	scanf("%c",&choice);
+	if(choice == 'q' || choice == 'Q')
+	{
+		return;
+	}
+	if(choice == '1')
+	{
+		system("cls");
+		display();
+	}
+}
